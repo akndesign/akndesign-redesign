489994257a841699f4cb65c59bcdaaa5e2f3397f
akndesign <alexanderkoziol@hotmail.com>
2022-07-13 01:52:03 -0400

    Updates

---
+++ precache-manifest.a0f8b436308e10c9a964a40479615843.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "edcd1404e251ecad6f9f",
+    "url": "/akndesign-redesign/static/css/main.6acec55b.chunk.css"
+  },
+  {
+    "revision": "edcd1404e251ecad6f9f",
+    "url": "/akndesign-redesign/static/js/main.4af3dde0.chunk.js"
+  },
+  {
+    "revision": "0b664de749a62543ad67",
+    "url": "/akndesign-redesign/static/js/runtime~main.59b5ec5c.js"
+  },
+  {
+    "revision": "0336f628a1d40cf52cbd",
+    "url": "/akndesign-redesign/static/js/2.651df3a4.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/akndesign-redesign/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "532d16ede868a12664cec0af261fbcbc",
+    "url": "/akndesign-redesign/static/media/clippy.532d16ed.png"
+  },
+  {
+    "revision": "68411bba0cb063dfad293103b79244ee",
+    "url": "/akndesign-redesign/static/media/The Devil's Work.68411bba.ttf"
+  },
+  {
+    "revision": "2b38b31a60bd6636efeb6e416c0a95ee",
+    "url": "/akndesign-redesign/static/media/hello.2b38b31a.ttf"
+  },
+  {
+    "revision": "487c46655ea91c50b2a3c3c39c630060",
+    "url": "/akndesign-redesign/static/media/arrow-right.487c4665.svg"
+  },
+  {
+    "revision": "2b02343f44f9ea08861232894db695fc",
+    "url": "/akndesign-redesign/static/media/elgoc-bold-webfont.2b02343f.woff"
+  },
+  {
+    "revision": "2f1506318dce10c7a9e3f106828602b3",
+    "url": "/akndesign-redesign/static/media/flower.2f150631.svg"
+  },
+  {
+    "revision": "3bc4dae59d6d86f489dee52870fc33ce",
+    "url": "/akndesign-redesign/static/media/checkers.3bc4dae5.svg"
+  },
+  {
+    "revision": "ad10bff9b8fe5ebd79fdb7dd55e92b58",
+    "url": "/akndesign-redesign/static/media/checkers_yellow.ad10bff9.svg"
+  },
+  {
+    "revision": "0afb67bbda49e7fa8f8e6515f1a13680",
+    "url": "/akndesign-redesign/static/media/GT-Alpina-Condensed-Bold-Italic-Trial.0afb67bb.otf"
+  },
+  {
+    "revision": "d72df232d9dede798ad8b04944c15b80",
+    "url": "/akndesign-redesign/static/media/GT-America-Standard-Bold-Trial.d72df232.otf"
+  },
+  {
+    "revision": "81942b57169bf956b6ed20a16a298fce",
+    "url": "/akndesign-redesign/static/media/Bogue-Semibold.81942b57.ttf"
+  },
+  {
+    "revision": "04b95b08b05bceee55cfd172443d7825",
+    "url": "/akndesign-redesign/static/media/c_Misfits_30082019_Shot_10_802.04b95b08.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/akndesign-redesign/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e2010cae81c815919232f462b479ddb6",
+    "url": "/akndesign-redesign/static/media/Windows95.e2010cae.webp"
+  },
+  {
+    "revision": "9de66fe4842edf59e46e15c90e686087",
+    "url": "/akndesign-redesign/static/media/fixedsys.9de66fe4.ttf"
+  },
+  {
+    "revision": "90b85f007c9fbd0ff03a353dcc9fad83",
+    "url": "/akndesign-redesign/static/media/meangirls.90b85f00.mp4"
+  },
+  {
+    "revision": "4c3e5a9c73e454c43185aac489b00b18",
+    "url": "/akndesign-redesign/static/media/space-odyssey-2.4c3e5a9c.mp4"
+  },
+  {
+    "revision": "c5827a8a4a7fa3c9a8e64618a8a18966",
+    "url": "/akndesign-redesign/static/media/luke.c5827a8a.gif"
+  },
+  {
+    "revision": "f8c8e0ac962bb64a4fa593c797706dcb",
+    "url": "/akndesign-redesign/index.html"
+  }
+];
